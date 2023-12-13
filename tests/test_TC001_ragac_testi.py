9620f518bebd3ffec216a478b742a89f181c3c60
tbarami <89800402+tbarami@users.noreply.github.com>
2023-12-13 18:26:05 +0400

    Add files via upload

--- tests/test_TC001_ragac_testi.py
+++ tests/test_TC001_ragac_testi.py
@@ -1,9 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
 # Initialize the WebDriver (using Chrome in this example)
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver = webdriver.Chrome()
 
 # Open Google
 driver.get("http://www.google.com")
